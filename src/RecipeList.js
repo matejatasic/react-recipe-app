14fb7f6e749e440712b435d97fd56ab26c9ef329
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2022-01-18 14:06:00 +0100

    Update RecipeList.js

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,10 +4,12 @@ import Recipe from './Recipe';
 export default class RecipeList extends Component {
     render() {
         return (
-            <div>
-              {this.props.recipes.map((recipe, i) => {
-                  return <Recipe key={i} recipe={recipe} />
-              })}  
+            <div id="recipe-list">
+                {
+                    this.props.recipes.map((recipe, i) => {
+                        return <Recipe key={i} recipe={recipe} />
+                    })
+                }  
             </div>
         );
     }
